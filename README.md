12a675da86eb89a43aba5ef19eb4320b3cd24b59
3hlujy <1173908229@qq.com>
2024-04-26 15:26:34 +0800

    萤石云播放器

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # video-plug
 
 # 萤石云的视频插件
+官网：https://open.ys7.com/help/532
+
 
 # 海康H5视频播放插件
+官网 ： https://open.hikvision.com/download/5c67f1e2f05948198c909700?type=20
 
 # 海康视频WEB插件
+官网 ： https://open.hikvision.com/download/5c67f1e2f05948198c909700?type=20
---
+++ 萤石云播放器/ezuikitThemeData.js
@@ -0,0 +1,117 @@
+let themeData = {
+  "header": {
+    "color": "#1890ff",
+    "activeColor": "#FFFFFF",
+    "backgroundColor": "#000000",
+    "btnList": [
+      // {
+      //   "iconId": "deviceID",
+      //   "part": "left",
+      //   "defaultActive": 0,
+      //   "memo": "顶部设备序列号",
+      //   "isrender": 1
+      // },
+      // {
+      //   "iconId": "deviceName",
+      //   "part": "left",
+      //   "defaultActive": 0,
+      //   "memo": "顶部设备名称",
+      //   "isrender": 1
+      // },
+      {
+        "iconId": "cloudRec",
+        "part": "right",
+        "defaultActive": 0,
+        "memo": "云存储",
+        "isrender": 0
+      },
+      {
+        "iconId": "rec",
+        "part": "right",
+        "defaultActive": 0,
+        "memo": "SD卡回放",
+        "isrender": 0
+      }
+    ]
+  },
+  "footer": {
+    "color": "#FFFFFF",
+    "activeColor": "#1890FF",
+    "backgroundColor": "#00000021",
+    "btnList": [
+      {
+        "iconId": "play",
+        "part": "left",
+        "defaultActive": 1,
+        "memo": "播放",
+        "isrender": 1
+      },
+      {
+        "iconId": "capturePicture",
+        "part": "left",
+        "defaultActive": 0,
+        "memo": "截屏按钮",
+        "isrender": 1
+      },
+      {
+        "iconId": "sound",
+        "part": "left",
+        "defaultActive": 0,
+        "memo": "声音按钮",
+        "isrender": 1
+      },
+      {
+        "iconId": "pantile",
+        "part": "left",
+        "defaultActive": 0,
+        "memo": "云台控制按钮",
+        "isrender": 1
+      },
+      {
+        "iconId": "recordvideo",
+        "part": "left",
+        "defaultActive": 0,
+        "memo": "录制按钮",
+        "isrender": 1
+      },
+      {
+        "iconId": "talk",
+        "part": "left",
+        "defaultActive": 0,
+        "memo": "对讲按钮",
+        "isrender": 1
+      },
+      // {
+      //   "iconId": "zoom",
+      //   "part": "left",
+      //   "defaultActive": 0,
+      //   "memo": "电子放大",
+      //   "isrender": 1
+      // },
+      {
+        "iconId": "hd",
+        "part": "right",
+        "defaultActive": 0,
+        "memo": "清晰度切换按钮",
+        "isrender": 1
+      },
+      {
+        "iconId": "webExpend",
+        "part": "right",
+        "defaultActive": 0,
+        "memo": "网页全屏按钮",
+        "isrender": 1
+      },
+      // {
+      //   "iconId": "expend",
+      //   "part": "right",
+      //   "defaultActive": 0,
+      //   "memo": "全局全屏按钮",
+      //   "isrender": 1
+      // }
+    ]
+  }
+};
+export default {
+  themeData
+}
